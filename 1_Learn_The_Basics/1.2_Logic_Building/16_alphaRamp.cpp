42d67bcb1b455f6d667d1219a85669630cd54f16
DeependraParmar <deependraparmar1@gmail.com>
2023-12-29 14:52:51 +0530

    Alpha Ramp

---
+++ 1_Learn_The_Basics/1.2_Logic_Building/16_alphaRamp.cpp
@@ -0,0 +1,12 @@
+/*
+void alphaRamp(int n) {
+    char ch = 'A';
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=i; j++){
+            cout << char(ch + i) << " ";
+        }
+        cout << endl;
+    }
+}
+
+*/
