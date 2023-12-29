4d0581943914114be2e1a0ccf17752b433392c34
DeependraParmar <deependraparmar1@gmail.com>
2023-12-29 12:27:27 +0530

    If Else Statements

---
+++ 1_Learn_The_Basics/1.1_Know_The_Language/03_ifElseStatements.cpp
@@ -0,0 +1,14 @@
+/*
+string compareIfElse(int a, int b) {
+    if(a<b){
+        return "smaller";
+    }
+    else if(a>b){
+        return "greater";
+    }
+    else{
+        return "equal";
+    }
+}
+
+*/
