92471cc8b13d713e82bfa03903a3a4577b06f4e5
DeependraParmar <deependraparmar1@gmail.com>
2023-12-29 16:55:45 +0530

    Check whether a number is prime or not

---
+++ 1_Learn_The_Basics/1.4_Basic_Math/07_primeNumber.cpp
@@ -0,0 +1,18 @@
+/*
+#include <math.h>
+bool isPrime(int n)
+{
+    int sq = sqrt(n);
+
+    if(n==1)
+        return false;
+
+    for(int i=2; i<=sq; i++){
+        if(n%i==0)
+            return false;
+    }
+
+    return true;
+}
+
+*/
