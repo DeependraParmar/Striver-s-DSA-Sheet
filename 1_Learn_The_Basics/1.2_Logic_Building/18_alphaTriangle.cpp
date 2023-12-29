2778502a755a1bae4505e6660743e3cbd1b85d6a
DeependraParmar <deependraparmar1@gmail.com>
2023-12-29 14:53:09 +0530

    Alpha Triangle

---
+++ 1_Learn_The_Basics/1.2_Logic_Building/18_alphaTriangle.cpp
@@ -0,0 +1,13 @@
+/*
+void alphaTriangle(int n) {
+    char ch = 'A';
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            char ch1 = ch+n-j;
+            cout << ch1 << " ";
+        }
+        cout << endl;
+    }
+}
+*/
