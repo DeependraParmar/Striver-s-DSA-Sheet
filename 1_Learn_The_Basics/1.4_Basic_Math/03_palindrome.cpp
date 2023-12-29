126e86223c724a7595d8d44e9b85949194fd144e
DeependraParmar <deependraparmar1@gmail.com>
2023-12-29 15:55:56 +0530

    Check Palindrome

---
+++ 1_Learn_The_Basics/1.4_Basic_Math/03_palindrome.cpp
@@ -0,0 +1,20 @@
+/*
+int reverse(int n){
+    int rev = 0;
+    while(n!=0){
+        int digit = n%10;
+        rev = (rev*10) + digit;
+        n/=10;
+    }
+
+    return rev;
+}
+bool palindrome(int n)
+{
+    if(n == reverse(n)){
+        return true;
+    }
+
+    return false;
+}
+*/
