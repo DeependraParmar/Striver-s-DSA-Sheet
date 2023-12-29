058727d5454facc557fe3d6409c96671ed0927be
DeependraParmar <deependraparmar1@gmail.com>
2023-12-29 15:53:10 +0530

    Count Bits

---
+++ 1_Learn_The_Basics/1.4_Basic_Math/01_countDigits.cpp
@@ -0,0 +1,19 @@
+/*
+int countDigits(int n){
+    int count = 0;
+    int num = n;
+
+    while(num > 0){
+        int digit = num % 10;
+
+        if(digit != 0 && n % digit == 0){
+            count++;
+        }
+
+        num /= 10;
+    }
+
+    return count;
+}
+
+*/
