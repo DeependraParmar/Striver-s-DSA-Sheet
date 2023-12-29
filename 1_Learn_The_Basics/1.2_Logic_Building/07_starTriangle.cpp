0f1c53c883bb561a5d87c068cb577c7d4cbc53f4
DeependraParmar <deependraparmar1@gmail.com>
2023-12-29 14:51:06 +0530

    Star Triangle

---
+++ 1_Learn_The_Basics/1.2_Logic_Building/07_starTriangle.cpp
@@ -0,0 +1,16 @@
+/*
+void nStarTriangle(int n) {
+    for(int i=0; i<n; i++){
+        // printing the spaces first
+        for(int j=1; j<n-i; j++){
+           cout << " ";
+        }
+        // priting the stars
+        for(int j=0; j<2*i+1; j++){
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+}
+*/
