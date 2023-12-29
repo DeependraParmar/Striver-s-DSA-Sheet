11981dfc41f876128e6c47b2ede49c6101f4e917
DeependraParmar <deependraparmar1@gmail.com>
2023-12-29 12:33:08 +0530

    Switch Case

---
+++ 1_Learn_The_Basics/1.1_Know_The_Language/04_switchCase.cpp
@@ -0,0 +1,12 @@
+/*
+# define M_PI           3.14159265358979323846
+double areaSwitchCase(int ch, vector<double> a) {
+    switch(ch){
+        case 1:
+            return double(M_PI*a[0]*a[0]);
+        case 2:
+            return double(a[0]*a[1]);
+    }
+}
+
+*/
