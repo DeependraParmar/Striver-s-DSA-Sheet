9af5632d2299f35119a548a05ae625e68a388ae4
DeependraParmar <deependraparmar1@gmail.com>
2023-12-29 14:51:21 +0530

    Reverse Star Triangle

---
+++ 1_Learn_The_Basics/1.2_Logic_Building/08_reverseStarTriangle.cpp
@@ -0,0 +1,13 @@
+/*
+void nStarTriangle(int n) {
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<i; j++){
+            cout << " ";
+        }
+        for(int j=0; j<2*(n-i)+1; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+*/
