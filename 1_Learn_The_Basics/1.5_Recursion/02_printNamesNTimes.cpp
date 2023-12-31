1c6e5e0e99c9fd054b9cab658458b073f6d46350
DeependraParmar <deependraparmar1@gmail.com>
2023-12-31 11:39:32 +0530

    Print Name n times

---
+++ 1_Learn_The_Basics/1.5_Recursion/02_printNamesNTimes.cpp
@@ -0,0 +1,11 @@
+/*
+vector<string> printNTimes(int n) {
+    vector<string> ans;
+    if(n==0)
+        return ans;
+
+    ans = printNTimes(n-1);
+    ans.push_back("Coding Ninjas");
+    return ans;
+}
+*/
