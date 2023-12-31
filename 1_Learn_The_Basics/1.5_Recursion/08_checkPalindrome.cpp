ace3fec8c0fca1982e450741627eb725aeb51cb4
DeependraParmar <deependraparmar1@gmail.com>
2023-12-31 15:00:49 +0530

    Check Palindrome Coding Ninjas

---
+++ 1_Learn_The_Basics/1.5_Recursion/08_checkPalindrome.cpp
@@ -0,0 +1,19 @@
+/*
+bool isPalindrome(string& str) {
+    int start = 0;
+    int end = str.length() - 1;
+
+    while(start <= end){
+      if (str[start] == str[end]) {
+        start++;
+        end--;
+      }
+      else {
+        return false;
+      }
+    }
+
+    return true;
+}
+
+*/
