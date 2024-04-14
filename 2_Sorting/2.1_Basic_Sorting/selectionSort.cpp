270bab1e04f9381d85cd3427fb318ca21713e59e
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-04-14 13:10:39 +0530

    Selection sort

---
+++ 2_Sorting/2.1_Basic_Sorting/selectionSort.cpp
@@ -0,0 +1,15 @@
+/*
+void selectionSort(vector<int>&arr) {
+    for(int i=0; i<arr.size()-1; i++){
+        int min = i;
+
+        for(int j=i+1; j<arr.size(); j++){
+            if(arr[min] > arr[j])
+                min = j;
+        }
+
+        if(min != i)
+            swap(arr[min], arr[i]);
+    }
+}
+*/
