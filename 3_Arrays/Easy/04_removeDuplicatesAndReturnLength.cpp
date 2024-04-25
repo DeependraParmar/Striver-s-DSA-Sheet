bade502ffe10139bb7cd05b8a90b584bc21eb500
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-04-25 11:54:36 +0530

    Remove duplicated and return updated array length

---
+++ 3_Arrays/Easy/04_removeDuplicatesAndReturnLength.cpp
@@ -0,0 +1,12 @@
+/*
+int removeDuplicates(vector<int> &arr, int n) {
+    for(int i=0; i< arr.size() -1 ; i++){
+        if(arr[i+1] == arr[i]){
+            arr.erase(arr.begin() + i);
+            i--;
+        }
+    }
+
+    return arr.size();
+}
+*/
