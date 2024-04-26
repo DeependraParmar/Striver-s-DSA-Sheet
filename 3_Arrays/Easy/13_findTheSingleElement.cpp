368273907ba66a212e7dd84bac9710e841241944
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-04-26 05:46:05 +0530

    Find the single element

---
+++ 3_Arrays/Easy/13_findTheSingleElement.cpp
@@ -0,0 +1,11 @@
+/*
+#include<vector>
+
+int getSingleElement(vector<int> &arr){
+    int n = arr.size();
+    for(int i=0; i<n; i+=2){
+        if(arr[i] != arr[i+1])
+            return arr[i];
+    }
+}
+*/
