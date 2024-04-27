207c2b4c491b1eb72291148d0079e1f7072994fa
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-04-27 11:02:16 +0530

    Sort 0's, 1's and 2's: counting frequency

---
+++ 3_Arrays/Easy/17_sort0,1,&2.cpp
@@ -0,0 +1,18 @@
+/*
+#include <bits/stdc++.h>
+void sortArray(vector<int>& arr, int n)
+{
+    int count[3] = {0};
+
+    for(int i=0; i<n; i++){
+        count[arr[i]]++;
+    }
+
+    int index = 0;
+    for(int i=0; i<3; i++){
+        for(int j=0; j<count[i]; j++)
+            arr[index++] = i;
+    }
+}
+
+*/
